add23f848b33b913189b22f8f63d917e862e8a12
Ashutosh <Ashutosh.manshrestha@student.vinci.be>
2024-11-01 12:19:37 +0100

    new : ex 2.5

---
+++ exercices/2.5/src/components/Counter/index.tsx
@@ -0,0 +1,29 @@
+import {useState} from "react"
+export interface props{ 
+    titre:string;
+    message:string;
+    hoverMessage:string;
+}
+export const ClickCounter= ({titre, message, hoverMessage}:props)=>{
+    const [count, setCount] = useState(0);
+    const [hovered, sethovered] = useState(false);
+    
+
+    return(
+        <div className="card">
+            <h3>{titre}</h3>
+            <p>{hovered ? hoverMessage : null}</p>
+            <button 
+                onClick={() => setCount((count) => count + 1)} 
+                onMouseEnter={() => sethovered(true)}
+                onMouseLeave={() => sethovered(false)} 
+            >
+
+                count is {count}
+            </button>  
+            <p>{count>=10 ? `${message} rien !` : message}</p>  
+        </div>
+    
+        
+    );
+}
